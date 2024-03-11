4f74c2620e4cb8975078a84b360c8f8d0499720c
Dhaneswar <dhaneswarbhakat2000@gmail.com>
2024-03-12 01:18:09 +0530

    Done from home.

---
+++ lib/Screens/CustomAppBar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+
+  AnimationController animationController;
+  Animation colorTween, homeTween, yogaTween, iconTween, drawerTween;
+  Function()? onPressed;
+
+  CustomAppBar({
+    required this.animationController,
+    required this.colorTween,
+    required this.drawerTween,
+    required this.homeTween,
+    required this.iconTween,
+    required this.onPressed,
+    required this.yogaTween
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: AnimatedBuilder(
+        animation: animationController,
+        builder: (context,child)=>AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.dehaze,
+              color: drawerTween.value,
+            ),
+            onPressed: onPressed,
+
+          ),
+          backgroundColor: colorTween.value,
+          elevation: 0,
+          title: Row(
+            children: [
+              Text("HOME ", style: TextStyle(
+                color: homeTween.value,
+                fontWeight: FontWeight.bold,
+                fontSize: 20
+              ),),
+              Text("YOGA", style: TextStyle(
+                  color: yogaTween.value,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+              ),)
+            ],
+          ),
+          actions: [
+            Icon(Icons.notifications, color: iconTween.value,),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                backgroundColor: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,97 +1,75 @@
 //Main Screen page
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:yoga_app/Screens/CustomAppBar.dart';
+
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+
+  late AnimationController _animationController;
+  late Animation _colorTween, _homeTween,_yogaTween,_iconTween,_drawerTween;
+  late AnimationController _textAnimationController;
+
+
+  @override
+  void initState() {
+    _animationController = AnimationController(vsync: this,duration: Duration(seconds: 0));
+    _colorTween = ColorTween(begin: Colors.transparent, end: Colors.white).animate(_animationController);
+    _iconTween = ColorTween(begin: Colors.white, end: Colors.lightBlue).animate(_animationController);
+    _drawerTween = ColorTween(begin: Colors.white, end: Colors.black).animate(_animationController);
+    _homeTween = ColorTween(begin: Colors.white, end: Colors.blue).animate(_animationController);
+    _yogaTween = ColorTween(begin: Colors.white, end: Colors.blue).animate(_animationController);
+    _textAnimationController = AnimationController(vsync: this, duration: Duration(seconds: 0));
+    super.initState();
+  }
+
+
+  bool scrollListner(ScrollNotification scrollNotification){
+    bool scroll = false;
+    if(scrollNotification.metrics.axis == Axis.vertical){
+      _animationController.animateTo(scrollNotification.metrics.pixels/80);
+      _textAnimationController.animateTo(scrollNotification.metrics.pixels);
+      return scroll = true;
+    }
+      return scroll;
+  }
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text("Indian Youga App"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
+    return Scaffold(
+      key: scaffoldKey,
+      drawer: Drawer(),
+      backgroundColor: Colors.white,
+      body: NotificationListener(
+        onNotification: scrollListner,
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Text("1"),
-                      Text("Strick"),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Text("1"),
-                      Text("Strick"),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Divider(thickness: 3, indent: 20, endIndent: 20,),
             Container(
-              padding: EdgeInsets.all(25),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Yoga For All"),
-                    Padding(
-                      padding: const EdgeInsets.symmetric( vertical: 5),
-                      child: Container(
-                        color: Colors.blueGrey,
-                        height: 150,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric( vertical: 5),
-                      child: Container(
-                        color: Colors.blueGrey,
-                        height: 150,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            Container(
-              padding: EdgeInsets.all(25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              height: double.infinity,
+              child: Stack(
                 children: [
-                  Text("Yoga For Students"),
-                  Padding(
-                    padding: const EdgeInsets.symmetric( vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
+                  SingleChildScrollView(
+                    child: Stack(
+                      children: [
+                        Column(
+                          children: [
+                            Container(height: 1000, color: Colors.blue,),
+                            Container(height: 1000, color: Colors.green,),
+                            Container(height: 1000, color: Colors.blue,),
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric( vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric( vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric( vertical: 5),
-                    child: Container(
-                      color: Colors.blueGrey,
-                      height: 150,
-                    ),
-                  )
+                  CustomAppBar(animationController: _animationController, colorTween: _colorTween, drawerTween: _drawerTween, homeTween: _homeTween, iconTween: _iconTween, onPressed: (){scaffoldKey.currentState?.openDrawer();}, yogaTween: _yogaTween)
                 ],
               ),
             )
@@ -101,3 +79,99 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//         title: Text("Indian Youga App"),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             Padding(
+//               padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 25),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Column(
+//                     children: [
+//                       Text("1"),
+//                       Text("Strick"),
+//                     ],
+//                   ),
+//                   Column(
+//                     children: [
+//                       Text("1"),
+//                       Text("Strick"),
+//                     ],
+//                   )
+//                 ],
+//               ),
+//             ),
+//             Divider(thickness: 3, indent: 20, endIndent: 20,),
+//             Container(
+//               padding: EdgeInsets.all(25),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Text("Yoga For All"),
+//                     Padding(
+//                       padding: const EdgeInsets.symmetric( vertical: 5),
+//                       child: Container(
+//                         color: Colors.blueGrey,
+//                         height: 150,
+//                       ),
+//                     ),
+//                     Padding(
+//                       padding: const EdgeInsets.symmetric( vertical: 5),
+//                       child: Container(
+//                         color: Colors.blueGrey,
+//                         height: 150,
+//                       ),
+//                     )
+//                   ],
+//                 ),
+//               ),
+//             Container(
+//               padding: EdgeInsets.all(25),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Text("Yoga For Students"),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric( vertical: 5),
+//                     child: Container(
+//                       color: Colors.blueGrey,
+//                       height: 150,
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric( vertical: 5),
+//                     child: Container(
+//                       color: Colors.blueGrey,
+//                       height: 150,
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric( vertical: 5),
+//                     child: Container(
+//                       color: Colors.blueGrey,
+//                       height: 150,
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric( vertical: 5),
+//                     child: Container(
+//                       color: Colors.blueGrey,
+//                       height: 150,
+//                     ),
+//                   )
+//                 ],
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
